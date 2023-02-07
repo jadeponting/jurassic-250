2b7d822ebeb3cff36787c8fe9a01320fbc8e0425
Jade Ponting <jade@tandsadvertising.co.uk>
2023-02-07 16:14:15 +0000

    Add Lottie and script

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://cdn.jsdelivr.net/gh/jadeponting/jurassic-250@main/Jan%20offer%20GDNs%2020230124%20-%20250x250.json'
+})
